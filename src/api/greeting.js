e8e798d95f5bd2134df3802fd71c3cdfd8284584
Owen Buckley <owenbuckley13@gmail.com>
2023-08-07 21:56:51 -0400

    basic form setup and response messaging

--- src/api/greeting.js
+++ src/api/greeting.js
@@ -1,7 +1,6 @@
 import { getMessage } from '../services/message.js';
 
 export async function handler(request) {
-  console.log({ request });
   const params = new URLSearchParams(request.url.slice(request.url.indexOf('?')));
   const name = params.has('name') ? params.get('name') : 'Greenwood';
   const body = getMessage(name);
