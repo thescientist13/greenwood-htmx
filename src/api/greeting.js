20b672d9c58085e1899275456735d81f5a72103a
Owen Buckley <owenbuckley13@gmail.com>
2023-08-08 20:42:57 -0400

    more vercel debugging

--- src/api/greeting.js
+++ src/api/greeting.js
@@ -1,12 +1,15 @@
-import { getMessage } from '../services/message.js';
-
 export async function handler(request) {
-  const params = new URLSearchParams(request.url.slice(request.url.indexOf('?')));
-  const name = params.has('name') ? params.get('name') : 'Greenwood';
-  const body = getMessage(name);
-  const headers = new Headers({
-    'Content-Type': 'text/html'
-  });
+  console.log('type of', typeof request.body);
+  console.log('BODY', request.body);
+  // const formData = new FormData(request.body);
+  const formData = new Set(request.body);
+  // console.log({ formData });
+  const name = formData.has('name') ? formData.get('name') : 'Greenwood';
+  const body = `Hello ${name}! 👋`;
 
-  return new Response(body, { headers });
+  return new Response(body, {
+    headers: new Headers({
+      'Content-Type': 'text/html'
+    })
+  });
 }
