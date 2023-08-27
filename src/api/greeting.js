2fbf35b018c66229d590cff16b82c5f636dfcd3a
Owen Buckley <owenbuckley13@gmail.com>
2023-08-27 13:32:01 -0400

    upgrade latest greenwood v0.29.0-rc.3 with FormData support

--- src/api/greeting.js
+++ src/api/greeting.js
@@ -1,9 +1,6 @@
 export async function handler(request) {
-  console.log('API BODY@@@@', request.body);
-  // TODO const formData = await request.formData()
-  const formData = await request.json();
-  console.log({ formData });
-  const name = formData.name ? formData.name : 'Greenwood';
+  const formData = await request.formData();
+  const name = formData.has('name') ? formData.get('name') : 'Greenwood';
   const body = `Hello ${name}! 👋`;
 
   return new Response(body, {
