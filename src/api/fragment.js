6d39585bef37318f57408b995db2a15714e6f0e3
Owen Buckley <owenbuckley13@gmail.com>
2023-09-01 10:39:39 -0400

    use products placeholder data for demos

--- src/api/fragment.js
+++ src/api/fragment.js
@@ -1,29 +1,29 @@
 import { renderFromHTML } from 'wc-compiler';
-import { getArtists } from '../services/artists.js';
+import { getProducts } from '../services/products.js';
 
 export async function handler(request) {
   const limit = 5;
   const params = new URLSearchParams(request.url.slice(request.url.indexOf('?')));
   const offset = params.has('offset') ? parseInt(params.get('offset'), 10) : 0;
-  const artists = (await getArtists()).slice(offset, offset + limit);
+  const products = (await getProducts()).slice(offset, offset + limit);
 
   const { html } = await renderFromHTML(`
     ${
-      artists.map((item, idx) => {
-        const { name, imageUrl } = item;
+      products.map((item, idx) => {
+        const { title, thumbnail } = item;
         const scrollAttrs = idx === limit - 1
           ? `
               hx-get="/api/fragment?offset=${offset + limit}"
               hx-trigger="revealed"
-              hx-target="#artists-results"
+              hx-target="#products-results"
               hx-swap="beforeend"
             `
           :  '';
 
         return `
           <app-card
-            title="${offset + idx + 1}) ${name}"
-            thumbnail="${imageUrl}"
+            title="${offset + idx + 1}) ${title}"
+            thumbnail="${thumbnail}"
             ${scrollAttrs}
           ></app-card>
         `;
