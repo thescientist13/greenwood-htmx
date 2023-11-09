82d76fcdbbd8eb6ca50e128477370b33d2554972
Owen Buckley <owenbuckley13@gmail.com>
2023-11-08 21:28:11 -0500

    `import` CSS in Card component (#7)
    * WIP custom CSS imports
    
    * latest patch
    
    * it works
    
    * restore import-css plugin
    
    * remove extension from API route import meta assets
    
    * latest patches and TODOs
    
    * upgrade alpha.5
    
    * final release

--- greenwood.config.js
+++ greenwood.config.js
@@ -1,7 +1,9 @@
 import { greenwoodPluginAdapterVercel } from '@greenwood/plugin-adapter-vercel';
+import { greenwoodPluginImportCss } from '@greenwood/plugin-import-css';
 
 export default {
   plugins: [
+    greenwoodPluginImportCss(),
     greenwoodPluginAdapterVercel()
   ]
 };
